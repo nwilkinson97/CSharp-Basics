58024c76d0be600f03ccfb34590d71d92f89e380
nwilkinson97 <nathan.wilkinson@grifols.com>
2018-10-25 13:19:36 -0400

    Implemented New Class
    This class is implemented in the Program.cs file.  This is not a good practice to store Classes for the projects you are working on.
    
    Snippet:  class
    Description:  Creates a new Class
    
    Shortcut:  {CTRL} + E + C
    Description:  Comment Current Line Out
    
    Shortcut:  {CTRL} + E + U
    Description:  Uncomment Current Line

--- Program.cs
+++ Program.cs
@@ -1,10 +1,45 @@
 ﻿namespace CSharp_Basics
 {
+    class Person
+    {
+	   public string Name;
+
+	   public void Introduce(string to)
+	   {
+		  System.Console.WriteLine("Hello {0} I am {1}", to ,Name);
+	   }
+
+	   public static Person Parse(string str)
+	   {
+		  var person = new Person();
+		  person.Name = str;
+
+		  return person;
+	   }
+    }
+
     class Program
     {
 	   static void Main(string[] args)
 	   {
-		  System.Console.WriteLine("Hello World!");
+		  // Basic Class Object Instatiated
+		  var p1 = new Person();
+		  p1.Name = "Thing 1";
+		  p1.Introduce("Thing 2");
+
+		  //  Using Statics which can be accessed via 
+		  //  the object and not an instance.
+		  var p2 = Person.Parse("Thing King");
+		  p2.Introduce("Super Thing");
+
+		  var p3 = new Person()
+		  {
+			 Name = "Kid Dynamite"
+		  };
+
+		  p3.Introduce("Chocolate Thunda");
+
+		  
 	   }
     }
 }
