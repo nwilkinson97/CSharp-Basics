2a08fb291bff8bfcd5eaaa3587c979ad1b650c82
nwilkinson97 <nathan.wilkinson@grifols.com>
2018-10-25 13:36:51 -0400

    Moved Method from Main
    Highlight code and either use shortcut method or cut and paste into a method called "IntroductionToClasses"
    
    Shortcut:  {CTRL} + {DOT}
    Description:  Highlight code you want to move then use Keyboard shortcut.

--- Program.cs
+++ Program.cs
@@ -4,23 +4,28 @@
     {
 	   static void Main(string[] args)
 	   {
+		  IntroductionToClasses();
+	   }
+
+	   private static void IntroductionToClasses()
+	   {
 		  // Basic Class Object Instatiated
-		  var p1 = new Person();
-		  p1.Name = "Thing 1";
-		  p1.Introduce("Thing 2");
+		  var p1 = new Person(); 
+		  p1.Name = "Thing 1"; 
+		  p1.Introduce("Thing 2"); 
 
-		  //  Using Statics which can be accessed via
-		  //  the object and not an instance.
-		  var p2 = Person.Parse("Thing King");
-		  p2.Introduce("Super Thing");
+		  //  Using Statics which can be accessed via 
+		  //  the object and not an instance. 
+		  var p2 = Person.Parse("Thing King"); 
+		  p2.Introduce("Super Thing"); 
 
-		  //  Using the Object Initializer for the Person Class
-		  var p3 = new Person()
+		  //  Using the Object Initializer for the Person Class 
+		  var p3 = new Person() 
 		  {
-			 Name = "Kid Dynamite"
+			 Name = "Kid Dynamite" 
 		  };
 
-		  p3.Introduce("Chocolate Thunda");		  
+		  p3.Introduce("Chocolate Thunda"); 
 	   }
     }
 }
