46e5c71c6a12afa05842e587bb32fc1cd27cdfd9
nwilkinson97 <nathan.wilkinson@grifols.com>
2018-10-25 13:32:06 -0400

    Moved Class Person
    Moved Class Person to a file named Person.cs
    
    Shortcut:  {CTRL} + .
    Note:  {CTRL} + {DOT}
    Description:  Open the Smart Sense Window.
    Note:  Used to Move the class object to another file

---
+++ Person.cs
@@ -0,0 +1,20 @@
+﻿namespace CSharp_Basics
+{
+    class Person
+    {
+	   public string Name;
+
+	   public void Introduce(string to)
+	   {
+		  System.Console.WriteLine("Hello {0} I am {1}", to ,Name);
+	   }
+
+	   public static Person Parse(string str)
+	   {
+		  var person = new Person();
+		  person.Name = str;
+
+		  return person;
+	   }
+    }
+}
--- Program.cs
+++ Program.cs
@@ -1,23 +1,5 @@
 ﻿namespace CSharp_Basics
 {
-    class Person
-    {
-	   public string Name;
-
-	   public void Introduce(string to)
-	   {
-		  System.Console.WriteLine("Hello {0} I am {1}", to ,Name);
-	   }
-
-	   public static Person Parse(string str)
-	   {
-		  var person = new Person();
-		  person.Name = str;
-
-		  return person;
-	   }
-    }
-
     class Program
     {
 	   static void Main(string[] args)
@@ -27,19 +9,18 @@
 		  p1.Name = "Thing 1";
 		  p1.Introduce("Thing 2");
 
-		  //  Using Statics which can be accessed via 
+		  //  Using Statics which can be accessed via
 		  //  the object and not an instance.
 		  var p2 = Person.Parse("Thing King");
 		  p2.Introduce("Super Thing");
 
+		  //  Using the Object Initializer for the Person Class
 		  var p3 = new Person()
 		  {
 			 Name = "Kid Dynamite"
 		  };
 
-		  p3.Introduce("Chocolate Thunda");
-
-		  
+		  p3.Introduce("Chocolate Thunda");		  
 	   }
     }
 }
