3f7d77c42b91c17ab8de834df1adeeed0621a395
nwilkinson97 <nathan.wilkinson@grifols.com>
2018-10-25 14:15:13 -0400

    Class Constructors Best Practice
    Using Class Constructors to automatically assign values.  You want to keep your class constructors to a minimum and you do not want to repeat code.
    
    Shortcut:  {CTRL} + {DOT}
    Description:  Smart Tag in this case it is used to add teh using statement at the top of the file for System.Collection.Generic
    
    Shortcut:  {CTRL} + K + C
    Description:  Comment Line
    
    Shortcut:  {CTRL} + K + U
    Description:  Uncomment Line of Code

--- Customer.cs
+++ Customer.cs
@@ -1,22 +1,25 @@
-﻿namespace CSharp_Basics
+﻿using System.Collections.Generic;
+
+namespace CSharp_Basics
 {
     class Customer
     {
 	   public int Id;
 	   public string Name;
+	   public List<Order> Orders;
 
 	   public Customer()
 	   {
+		  Orders = new List<Order>();
 	   }
 
-	   public Customer(int id)
+	   public Customer(int id) : this()
 	   {
 		  this.Id = id;
 	   }
 
-	   public Customer(int id, string name)
+	   public Customer(int id, string name) : this(id)
 	   {
-		  this.Id = id;
 		  this.Name = name;
 	   }
     }
---
+++ Order.cs
@@ -0,0 +1,7 @@
+﻿namespace CSharp_Basics
+{
+    class Order
+    {
+
+    }
+}
