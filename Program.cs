d1a40ba1b1de7bb2a7cb7abf2e2eebec60883a00
nwilkinson97 <nathan.wilkinson@grifols.com>
2018-10-26 15:09:26 -0400

    Inheritance (Basics)
    This shows how to extend a class and inherit from another.

--- Program.cs
+++ Program.cs
@@ -2,9 +2,31 @@
 {
     partial class Program
     {
+
 	   static void Main(string[] args)
 	   {
-		  ClassBasicsImplementation();
+		  //// This Method Calls Methods from Phase 1:  Basics of Classes
+		  // ClassBasicsImplementation();
+
+		  Inhertance();
+	   }
+
+	   private static void Inhertance()
+	   {
+		  //  Instatiate Class Object Text
+		  //  This object inherits from the PresentationOjbect and the Text Class
+		  //  Because of this we can access properties and methods in both classes.
+		  //  This is because of Inheritance.
+		  var text = new Text();
+
+		  //  PresentationObject
+		  text.Width = 100;
+		  text.Height = 20;
+		  text.Copy();
+		  text.Duplicate();
+
+		  //  Text
+		  text.AddHyperlink("www.google.com");
 	   }
     }
 }
---
+++ Text.cs
@@ -0,0 +1,18 @@
+﻿using System;
+
+namespace CSharp_Basics
+{
+    partial class Program
+    {
+	   public class Text : PresentationObject
+	   {
+		  public int FontSize { get; set; }
+		  public string FontName { get; set; }
+
+		  public void AddHyperlink(string url)
+		  {
+			 Console.WriteLine("We added a link to {0}", url);
+		  }
+	   }
+    }
+}
